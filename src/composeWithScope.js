710791ecc55b98fc671a05a9e9c724df24e78fe3
Charles Hsieh <charles@codev.tw>
2017-04-29 23:02:29 +0800

    remove comment

--- src/composeWithScope.js
+++ src/composeWithScope.js
@@ -4,16 +4,6 @@ import enterScope from './enterScope'
 import leaveScope from './leaveScope'
 
 
-// 放在 props 會放漏掉
-// 用 Symbol 中間太容易漏傳
-// const scopeSym = Symbol()
-// const scopeSym = 'scopeSym'
-
-// TODO 一層一層 push 到 context.composeWithScopeStack 裡
-// TODO custmize storeKey?
-// TODO 分開處理 static expose and dynamic expose?
-
-
 function composeWithScope (...hocs) {
   return compose(
     enterScope,
